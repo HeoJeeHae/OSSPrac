8a2721c94ceecab48536a8bee12914717287fc8a
HeoJeeHae <hjh021124@dgu.ac.kr>
2022-10-02 19:11:21 +0900

    1

---
+++ ossprac.java
@@ -0,0 +1,16 @@
+package OSSPrac;
+import java.util.Scanner;
+public class ossprac {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("이름을 입력하세요: ");
+		String name = sc.next();
+		System.out.print("학번을 입력하세요: ");
+		int num = sc.nextInt();
+		System.out.println("\n"+ "<출력>");
+		System.out.println("이름: "+ name);
+		System.out.println("학번: "+ num);
+		sc.close();
+	}
+}
