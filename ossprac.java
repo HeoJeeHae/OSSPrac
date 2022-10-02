746c11c23d6a8dce6b07d4530425ad71bf732907
HeoJeeHae <hjh021124@dgu.ac.kr>
2022-10-02 19:15:13 +0900

    2

--- ossprac.java
+++ ossprac.java
@@ -8,9 +8,12 @@ public class ossprac {
 		String name = sc.next();
 		System.out.print("학번을 입력하세요: ");
 		int num = sc.nextInt();
+        System.out.print("학과를 입력하세요: ");
+		String ma = sc.next();
 		System.out.println("\n"+ "<출력>");
 		System.out.println("이름: "+ name);
 		System.out.println("학번: "+ num);
+        System.out.print("학과: "+ ma);
 		sc.close();
 	}
 }
